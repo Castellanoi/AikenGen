9b709517b38f184052b18e4f521c8c8eb05fbd41
Ivan Castellano <castellano.ivan.93@gmail.com>
2024-10-12 18:33:40 -0300

    modelo taskbar final

--- frontend/src/components/taskbar/Edit.js
+++ frontend/src/components/taskbar/Edit.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Button, MenuItem, MenuList, Paper, Popper, Fade, ClickAwayListener } from '@mui/material';
+import { Button, MenuItem, MenuList, Paper, Popper, Fade, ClickAwayListener, ListItemIcon, ListItemText, Typography, Divider } from '@mui/material';
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
+import ContentCutIcon from '@mui/icons-material/ContentCut';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import SearchIcon from '@mui/icons-material/Search';
+import FindReplaceIcon from '@mui/icons-material/FindReplace';
 
 export default function Edit({ open, anchorEl, onMenuOpen, onMenuClose, onMenuHover, onKeyDown }) {
   return (
@@ -28,9 +35,85 @@ export default function Edit({ open, anchorEl, onMenuOpen, onMenuClose, onMenuHo
             <Paper>
               <ClickAwayListener onClickAway={onMenuClose}>
                 <MenuList id="edit-menu" aria-labelledby="edit-button" autoFocusItem={open} onKeyDown={onKeyDown}>
-                  <MenuItem onClick={onMenuClose}>Copiar</MenuItem>
-                  <MenuItem onClick={onMenuClose}>Pegar</MenuItem>
-                  <MenuItem onClick={onMenuClose}>Cortar</MenuItem>
+                  <MenuItem onClick={onMenuClose}>
+                    <ListItemIcon>
+                      <UndoIcon fontSize='small' />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Deshacer
+                    </ListItemText>
+                    <Typography variant="body2" color="text.secondary">
+                      Ctrl+Z
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem onClick={onMenuClose}>
+                    <ListItemIcon>
+                      <RedoIcon fontSize='small' />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Rehacer
+                    </ListItemText>
+                    <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                      Ctrl+Shift+Z 
+                    </Typography>
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem onClick={onMenuClose}>
+                    <ListItemIcon>
+                      <ContentCutIcon fontSize='small' />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Cortar
+                    </ListItemText>
+                    <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                      Ctrl+X
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem onClick={onMenuClose}>
+                    <ListItemIcon>
+                      <ContentCopyIcon fontSize='small' />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Copiar
+                    </ListItemText>
+                    <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                      Ctrl+C
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem onClick={onMenuClose}>
+                    <ListItemIcon>
+                      <ContentPasteIcon fontSize='small' />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Pegar
+                    </ListItemText>
+                    <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                      Ctrl+V
+                    </Typography>
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem onClick={onMenuClose}>
+                    <ListItemIcon>
+                      <SearchIcon fontSize='small' />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Buscar
+                    </ListItemText>
+                    <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                      Ctrl+F
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem onClick={onMenuClose}>
+                    <ListItemIcon>
+                      <FindReplaceIcon fontSize='small' />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Reemplazar
+                    </ListItemText>
+                    <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                      Ctrl+H
+                    </Typography>
+                  </MenuItem>
                 </MenuList>
               </ClickAwayListener>
             </Paper>
--- frontend/src/components/taskbar/File.js
+++ frontend/src/components/taskbar/File.js
@@ -1,42 +1,99 @@
 import React from 'react';
-import { Button, MenuItem, MenuList, Paper, Popper, Fade, ClickAwayListener } from '@mui/material';
+import { Button, MenuItem, MenuList, Paper, Popper, Fade, ClickAwayListener, ListItemIcon, ListItemText, Typography, Divider } from '@mui/material';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import SaveIcon from '@mui/icons-material/Save';
+import SaveAsIcon from '@mui/icons-material/SaveAs';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 export default function File({ open, anchorEl, onMenuOpen, onMenuClose, onMenuHover, onKeyDown }) {
-  return (
-    <>
-      <Button
-        aria-controls={open ? 'file-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={onMenuOpen}
-        onMouseEnter={onMenuHover}
-        color="inherit"
-        sx={{ textTransform: 'none', fontWeight: 'bold' }}
-      >
-        Archivo
-      </Button>
-      <Popper
-        open={open}
-        anchorEl={anchorEl}
-        role={undefined}
-        placement="bottom-start"
-        transition
-        disablePortal
-      >
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={50}>
-            <Paper>
-              <ClickAwayListener onClickAway={onMenuClose}>
-                <MenuList id="file-menu" aria-labelledby="file-button" autoFocusItem={open} onKeyDown={onKeyDown}>
-                  <MenuItem onClick={onMenuClose}>Nuevo</MenuItem>
-                  <MenuItem onClick={onMenuClose}>Guardar</MenuItem>
-                  <MenuItem onClick={onMenuClose}>Cargar</MenuItem>
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Fade>
-        )}
-      </Popper>
-    </>
-  );
+    return (
+        <>
+            <Button
+                aria-controls={open ? 'file-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={onMenuOpen}
+                onMouseEnter={onMenuHover}
+                color="inherit"
+                sx={{ textTransform: 'none', fontWeight: 'bold' }}
+            >
+                Archivo
+            </Button>
+            <Popper
+                open={open}
+                anchorEl={anchorEl}
+                role={undefined}
+                placement="bottom-start"
+                transition
+                disablePortal
+            >
+                {({ TransitionProps }) => (
+                    <Fade {...TransitionProps} timeout={50}>
+                        <Paper>
+                            <ClickAwayListener onClickAway={onMenuClose}>
+                                <MenuList id="file-menu" aria-labelledby="file-button" autoFocusItem={open} onKeyDown={onKeyDown}>
+                                    <MenuItem onClick={onMenuClose}>
+                                        <ListItemIcon>
+                                            <AddBoxIcon fontSize='small' />
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Nuevo
+                                        </ListItemText>
+                                        <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                                            Ctrl+N
+                                        </Typography>
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem onClick={onMenuClose}>
+                                        <ListItemIcon>
+                                            <UploadFileIcon fontSize='small' />
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Cargar
+                                        </ListItemText>
+                                        <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                                            Ctrl+O
+                                        </Typography>
+                                    </MenuItem>
+                                    <MenuItem onClick={onMenuClose}>
+                                        <ListItemIcon>
+                                            <SaveIcon fontSize='small' />
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Guardar
+                                        </ListItemText>
+                                        <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                                            Ctrl+S
+                                        </Typography>
+                                    </MenuItem>
+                                    <MenuItem onClick={onMenuClose}>
+                                        <ListItemIcon>
+                                            <SaveAsIcon fontSize='small' />
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Guardar Como
+                                        </ListItemText>
+                                        <Typography variant="body2" color="text.secondary" sx={{ ml: 5 }}>
+                                            Ctrl+Shift+S
+                                        </Typography>
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem onClick={onMenuClose}>
+                                        <ListItemIcon>
+                                            <CloseIcon fontSize='small' />
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Salir
+                                        </ListItemText>
+                                    </MenuItem>
+                                </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Fade>
+                )}
+            </Popper>
+        </>
+    );
 }
